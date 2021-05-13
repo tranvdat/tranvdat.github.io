ebf777cc8395f355622d1651517eafd8763e61c6
Tran Dat <dat.tran2209@pirago.work>
2021-05-13 12:04:03 +0700

    jquery scroll

--- js/main.js
+++ js/main.js
@@ -1,8 +1,60 @@
 $(document).ready(function () {
+    /* ======= ScrollTo ======= */
+    $(".scrollTo").on("click", function (e) {
+        //store hash
+        var target = this.hash;
+
+        e.preventDefault();
+
+        $("body").scrollTo(target, 800, { offset: -60, axis: "y" });
+    });
+
+    /* ======= Fixed page nav when scrolled ======= */
+
+    const $about = document.getElementById("about");
+    const $skills = document.getElementById("skills");
+    const $workExp = document.getElementById("work-experience");
+
+    const $skillsScroll = $skills.offsetTop - 120;
+    const $workExpScroll = $workExp.offsetTop - 120;
+
+    const listTagLis = {
+        'about': document.querySelector('#aside .menu a[href="#about"]').parentElement,
+        'skills': document.querySelector('#aside .menu a[href="#skills"]').parentElement,
+        'workExp': document.querySelector('#aside .menu a[href="#work-experience"]').parentElement,
+        'contact': document.querySelector('#aside .menu a[href="#contact"]').parentElement,
+    };
+
+    $(window).on("scroll resize load", function () {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop < $skillsScroll) {
+            addClassActive('about');
+        }
+        else if (scrollTop >= $skillsScroll && scrollTop < $workExpScroll) {
+            addClassActive('skills');
+        }
+        else if (scrollTop >= $workExpScroll && scrollTop < 2000) {
+            addClassActive('workExp');
+        }
+        else if (scrollTop >= 2000) {
+            addClassActive('contact');
+        }
+    });
+
+    function addClassActive(classEl) {
+        for (const key in listTagLis) {
+            if(key !== classEl) {
+                listTagLis[key].classList.remove('active');
+            }
+        }
+        listTagLis[classEl].className = 'active';
+    }
+
     /* ======= Chart ========= */
 
     $(".chart-primary").easyPieChart({
-        barColor: '#00d1b2',
+        barColor: "#00d1b2",
         trackColor: "#fff",
         scaleColor: false,
         lineWidth: 8,
@@ -14,7 +66,7 @@ $(document).ready(function () {
     });
 
     $(".chart-danger").easyPieChart({
-        barColor: '#ff3860',
+        barColor: "#ff3860",
         trackColor: "#fff",
         scaleColor: false,
         lineWidth: 8,
@@ -26,7 +78,7 @@ $(document).ready(function () {
     });
 
     $(".chart-warning").easyPieChart({
-        barColor: '#ffdd57',
+        barColor: "#ffdd57",
         trackColor: "#fff",
         scaleColor: false,
         lineWidth: 8,
